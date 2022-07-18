cf5f687ca6138a7c01e01e9f84d8daf42c542171
Noel <noeljose@protonmail.com>
2022-07-18 13:57:35 +0530

    Satya Example shown

---
+++ src/routes/user/property/update/[id].svelte
@@ -0,0 +1,19 @@
+<script context="module">
+
+    export function load({pramas}){
+
+        return {
+            props : {
+                id : pramas.id
+            }
+        }
+    }
+
+</script>
+
+<script>
+    export let id
+</script>
+
+
+<h1>{id}</h1>
